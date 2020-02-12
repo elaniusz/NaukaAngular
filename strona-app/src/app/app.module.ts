76e258110f04ff4f229f3fd65cd28e45a81211ad
Marek Michalik <marekmichalik@Mac-mini-Marek.local>
2020-02-12 20:17:48 +0100

    Routing

--- strona-app/src/app/app.module.ts
+++ strona-app/src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StronaMaterialModule } from './strona-material/strona-material.module';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { TeamDetailComponent } from './team-detail/team-detail.component';
+import { RoutingModule } from './routing/routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuComponent,
+    HomeComponent,
+    TeamComponent,
+    ContactComponent,
+    TeamDetailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    StronaMaterialModule
+    StronaMaterialModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- strona-app/src/app/strona-material/strona-material.module.ts
+++ strona-app/src/app/strona-material/strona-material.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatMenuModule, MatMenuItem } from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule, } from '@angular/material/select';
+import {MatSelectModule } from '@angular/material/select';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 
@@ -19,7 +20,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   ],
   exports: [
     CommonModule, MatMenuModule, MatToolbarModule, MatIconModule, MatButtonModule, MatInputModule,
-    MatSelectModule, MatCardModule, MatFormFieldModule, BrowserAnimationsModule
+    MatSelectModule, MatCardModule, MatFormFieldModule, BrowserAnimationsModule, MatGridListModule
      ]
 })
 export class StronaMaterialModule { }
